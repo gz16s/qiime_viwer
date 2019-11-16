8c2b51d44544dfd11d839a284168691663ccf839
gz16s <57826649+gz16s@users.noreply.github.com>
2019-11-16 16:31:09 +0800

    upload

---
+++ 3062740066.py
@@ -0,0 +1,14 @@
+import sys
+import random
+from github import Github
+from IPython.core.display import HTML
+file_path = sys.argv[0]
+g = Github("gz16s", "2019gz123456")
+repo = g.get_repo("gz16s/qiime_viwer")
+file_type = file_path.split('.')[-1]
+newfile = str(int(random.random()*10**10))+"."+file_type
+contents = open(file_path, 'rb').read()
+repo.create_file(newfile, "upload", contents, branch="master")
+view_data = "<a href='https://view.qiime2.org/visualization/?type=html&src=https%3A%2F%2Fraw.githubusercontent.com%2Fgz16s%2Fqiime_viwer%2Fmaster%2F"+newfile+"' target='_blank'>在新窗口查看结果</a>"
+view_data += "<iframe width='100%', height='300px', src='https://view.qiime2.org/visualization/?type=html&src=https%3A%2F%2Fraw.githubusercontent.com%2Fgz16s%2Fqiime_viwer%2Fmaster%2F"+newfile+"' />"
+display(HTML(view_data))
